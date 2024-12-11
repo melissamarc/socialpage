99a33aeb42e115d4067db39dfce6327d77530bfe
Wilson Leonardo <melmarc1moreira@gmail.com>
2024-12-11 19:18:02 -0300

    socialpage completa

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,28 @@ import './App.css'
 function App() {
   return (
 
-<div className="container">
-   <div className='card'>
- teste
+    <div className="container">
+      <div className='card'>
+
+        <img src='https://avatars.githubusercontent.com/u/110108163?v=4' className='image'></img>
 
 
+        <p className='title'> Melissa Marcielo Moreira</p>
+        <p className='location'> São Paulo, SP - Brasil </p>
+        <p className='description'> Desenvolvedora Web e Mobile | Full-Stack</p>
+
+        <div className="button-section">
+          <button> <a href='https://github.com/melissamarc'> Github </a> </button>
+          <button> <a href='https://github.com/melissamarc'> Portfólio  </a></button>
+          <button> <a href='https://www.linkedin.com/in/melissa-marcielo-moreira-527994284'> Linkedin </a> </button>
+          <button>  <a href='https://www.instagram.com/melmarcielo'> Instagram </a> </button>
         </div>
-</div>
-  
-    
+      </div>
+    </div>
 
-   
+
+
+
 
   )
 }
